0214c25a37c73e2c724fbf542233669073ebc6b2
wangjiali123 <wangjiali123>
2024-01-18 14:45:13 +0800

    修改链接

---
+++ gitPushGithub.bat
@@ -0,0 +1,18 @@
+
+@echo off
+
+rem https://www.python100.com/html/111789.html
+
+set /a i=0
+:LoopStart
+  echo loop %i%
+  set /a i+=1
+
+  rem git add *
+  rem git commit -m "%date% %time%: Updated By test"
+  git push origin "master"
+  rem exit
+
+  if %i% leq 1 goto LoopStart
+
+
